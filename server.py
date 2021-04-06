8a215cda245b5b82002d57d1e7ca80fa924061e8
Marina-ui <77714654+Marina-ui@users.noreply.github.com>
2021-04-06 21:40:18 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -27,6 +27,7 @@ def main():
     if answer == 'обучение' and context['mode'] == 'new':
         #запускается сценарий обучения
         pass
+
     elif answer == 'игра с алисой' and context['mode'] == 'new':
         #создаём контекст игры, запрашиваем кол-во кучек
         context['mode'] = 'игра'
@@ -34,18 +35,34 @@ def main():
         return generate_response(req, 'Всегда рада с вами поиграть. Выберите количество кучек: одна или две')
 
     elif (answer == 'одна' or answer == 'одна кучка') and context['kuchki'] == 0 and context['mode'] == 'игра': #формируем игру на одну кучку
-        #записываем в контекст оставшиеся параметры, овучиваем правила
+        #записываем в контекст оставшиеся параметры, озвучиваем правила
         context['kuchki'] = 1
-        chips = r.randint(20, 30)       #диапазон камней
-        chips_out = r.randint(3, 5)     #максимальное колво забираемых камней
-        context['chips'] = chips
-        context['max_chips_out'] = chips_out
-        rules = f'Отлично. Вот правило: всего {chips} фишек в куче. Мы по очереди берём 1-{chips_out} фишек.' \
-                f'Выиграет тот, кто заберёт последнюю. Формат вашего ответа: Забираю n фишек. Давайте, я начну.'   #тут будет логика с функцией generate_motion
-        return generate_response(req, rules)
+        context['chips'] = r.randint(15, 30)  # диапазон камней
+        context['max_chips_out'] = r.randint(3, 5)  # максимальное колво забираемых камней
+        return generate_response(req, 'Одна, так одна. Вам объяснить правила?')
 
     elif (answer == 'две' or answer == 'две кучки') and context['kuchki'] == 0 and context['mode'] == 'игра': #формируем игру на две кучки
         pass
+
+    elif (answer == 'да' or answer == 'объясни') and context['mode'] == 'игра' and context['kuchki'] == 1:
+        #объясняем правила, Алиса делает первый ход
+        chips = context['chips']
+        chips_out = context['max_chips_out']
+        motion = generate_motion(1, chips, chips_out, first=True)
+        context['chips'] -= motion[1]
+        rules = f'Объясняю: всего {chips} фишек в куче. Мы по очереди берём 1-{chips_out} фишек.' \
+                f'Выиграет тот, кто заберёт последнюю. Давайте, я начну. Беру {motion[0]}. Осталось {motion[1]}.' \
+                f'Теперь ваш ход. Сколько фишек вы забираете?'
+        return generate_response(req, rules)
+
+    elif (answer == 'нет' or answer == 'не нужно') and context['mode'] == 'игра' and context['kuchki'] == 1:
+        #Алиса делает первый ход
+        chips = context['chips']
+        chips_out = context['max_chips_out']
+        motion = generate_motion(1, chips, chips_out, first=True)
+        context['chips'] -= motion[1]
+        text = f'Хорошо, тогда я начну. Беру {motion[0]}. Осталось {motion[1]}. Теперь ваш ход.'
+        return generate_response(req, text)
 
     #...
 
@@ -69,7 +86,30 @@ def generate_response(req, text, end_session=False):
 
 
 def generate_motion(kuchki, chips, chips_out, first=False):   #генерирует ход Алисы
-    pass
+    if kuchki == 1 and first:           #первым ходом создаём выигрышную позицию для игрока в игре с одной кучкой
+        ost = chips % (chips_out + 1)
+        if ost == 0:
+            motion = r.randint(1, chips_out)
+        elif ost > 1:
+            motion = r.randint(1, ost - 1)
+        else:
+            motion = r.randint(2, chips_out)
+
+    elif kuchki == 1 and chips > chips_out:        #пытаемся привести игрока в проигрышную позицию
+        ost = chips % (chips_out + 1)
+        if ost == 0:
+            motion = r.randint(1, chips_out)
+        else:
+            motion = ost
+
+    elif kuchki == 1 and chips < chips_out:    #берём последние
+        motion = chips
+
+    elif kuchki == 2:     #ходы при игре с двумя кучками
+        pass
+
+    chips -= motion
+    return (motion, chips)
 
 
 if __name__ == '__main__':
