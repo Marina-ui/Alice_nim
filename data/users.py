849a670da11aa838efd966c7c3748a6f2150c9b1
Zhmuda-Daniil <77479899+Zhmuda-Daniil@users.noreply.github.com>
2021-04-25 19:28:38 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -5,7 +5,7 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, unique=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     wins = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     defs = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
